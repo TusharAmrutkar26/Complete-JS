04f5edac7d7c317c2a35f6566ada7564991b6f4b
Tushar Kishor Amrutkar <tamrutkar2696@gmail.com>
2026-06-06 15:51:36 +0530

    Add files via upload

---
+++ Day2_Datatypes.js
@@ -0,0 +1,34 @@
+//Datatypes in JS
+
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 2);  we are using alert in nodejs,so it will not work, we can use console.log instead
+
+console.log(3+2); //code readbility should be good, so we can use space between operators
+
+console.log("TUSHAR"); //string datatype
+
+let name = "TUSHAR"; //string datatype   => in JS string also a one object
+let age = 20; //number datatype
+let isloggedin = true; //boolean datatype
+
+//primitives datatypes
+
+//number => 2 to power 53
+//bigInt =>
+    //string=>""
+//boolean = true/false
+//null => empty value/ standalone value
+//undefined => value is not assigned
+//symbol => unique
+
+
+//object
+
+console.log(typeof "Tushar");
+
+console.log(typeof age);
+
+console.log(typeof null); //object
+
+console.log(typeof undefined); //undefined
