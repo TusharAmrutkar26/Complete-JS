923c7065462738c4f0733643613affac8beee54b
Tushar Kishor Amrutkar <tamrutkar2696@gmail.com>
2026-06-06 15:52:39 +0530

    Add files via upload

---
+++ comparision.js
@@ -0,0 +1,17 @@
+//Day 3: Comparison (>,<, ==, ===, !=, !==)
+
+console.log(2>1);
+console.log(2<1);
+console.log(2>=1);
+console.log(2<=1);
+console.log(2==1);
+console.log(2!=1);
+console.log(2!==1);
+
+console.log("2">1)
+console.log("02">1)
+
+console.log(null>0); //false
+console.log(null==0); //false because null is considered as 0 in non-strict equality
+console.log(null>=0); //true
+//Value conversion proeblem here ,Due to this "NULL" ko 0 mein convert kar diya jata hain
