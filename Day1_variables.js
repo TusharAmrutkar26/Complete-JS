cfdaeaa2c8833fee5cc0c40c3cdf2598d520f386
Tushar Kishor Amrutkar <tamrutkar2696@gmail.com>
2026-06-06 15:40:17 +0530

    Add files via upload

---
+++ Day1_variables.js
@@ -0,0 +1,29 @@
+const accountId=112240150
+let accountEmail="Tushar@gmail.com"
+var accountPpassword= "23459"
+accountCity="dhule"
+let accountState;
+
+// accountId= 345632
+
+console.log(accountId);
+
+accountEmail="tushar265@gmail.com"
+accountPassword="54674"
+accountCity="Pune"
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+//We use let when we want to create a variable whose value can change later in the program.
+
+let city = "Dhule";
+
+city = "Kolhapur";
+
+console.log(city); // Kolhapur
+
+
+/* prefer not to use var
+because of issues in block scoppe and funtional  scope
+*/
