300308c2b9f2c5b8cd587392a11f2f30f08432ed
Tushar Kishor Amrutkar <tamrutkar2696@gmail.com>
2026-06-11 16:51:55 +0530

    Add files via upload

---
+++ json.js
@@ -0,0 +1,29 @@
+//JSON
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Hitesh Choudhary"
+}
+
+//course.courseInstructor
+
+const{courseInstructor: instructor} =course  ///object de-constructor
+console.log(instructor)
+
+//const navbar = ({company}) //de-structuring
+
+//API
+
+//{
+  //  name: "Tushar",
+    //course_name: "js in hindi",
+    //price: "free of cost"
+//}
+
+[
+    {},
+    {},
+    {}
+]
+
